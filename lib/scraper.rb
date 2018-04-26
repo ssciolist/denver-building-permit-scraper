a69e8d3f4051fe4f8a1a2f9a46788ec9ba1a5318
Megan Arellano <ssciolist@gmail.com>
2018-04-26 08:28:48 -0600

    can create links

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,2 +1,17 @@
 require 'mechanize'
 require 'open-uri'
+
+agent = Mechanize.new
+page = agent.get("https://www.denvergov.org/content/denvergov/en/denver-development-services/help-me-find-/building-permits.html")
+
+a_tags = page.css('li a[adhocenable="false"]')
+
+links = a_tags.map do |item|
+  "https://www.denvergov.org" + item['href']
+end
+
+puts links
+
+File.open('any_name_here.xls', 'wb') do |file|
+ file << open('http://www.example.com/xls_file.xls').read
+end
